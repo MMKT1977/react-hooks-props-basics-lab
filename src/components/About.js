21b899f7bcef91e880f18b90359c4643372c4072
MMKT1977 <kennedy.moranga@student.moringaschool.com>
2023-11-22 00:45:44 +0300

    Props lab

--- src/components/About.js
+++ src/components/About.js
@@ -9,9 +9,6 @@ function About({bio, links}) {
       <h2>About Me</h2>
       {bio && bio.length > 1 ? <p>{bio}</p> : null}
      
-     {/*<p>{props.bio}</p>; add if else statement to render or not if prop of bio is passed */}
-
-
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       
       
