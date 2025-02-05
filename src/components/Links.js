afe93fc6354e2c63bdb435def1b3d79c5ca5887c
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-02-05 23:57:09 +0300

    testing upload for codegrade

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,13 +1,11 @@
 import React from "react"
 
-
 function Links({ github, linkedin }){
     return(
     <div>
         <h3>Links</h3>
         <a href={github}>{github} </a>
         <a href={linkedin}>{linkedin}</a>
-
     </div>
     );
     
