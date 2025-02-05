7fb9bac3f65691e96c9f56e88bde7651076b4288
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-02-05 18:12:46 +0300

    passed links as props

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,12 +1,12 @@
 import React from "react"
 
 
-function Links(links){
+function Links({links}){
     return(
     <div>
         <h3>Links</h3>
-        <a href={links.github}>Github </a>
-        <a href={links.linkedin}>Linkedin </a>
+        <a href={links.github}>{links.github} </a>
+        <a href={links.linkedin}>{links.linkedin}</a>
     </div>
     )
     
